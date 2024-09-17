b9f539d0c282b57dcb4c1f7befeed0ccc2df1f07
yash2154 <yash2154rai@gmail.com>
2024-09-17 11:33:02 +0530

    fix bg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import Login from "@/components/logincomponent";
 
 export default function signup() {
   return (
-    <main className=" h-screen w-screen flex items-center justify-center ">
+    <main className=" h-screen w-screen flex items-center justify-center dark:bg-zinc-950 bg-slate-200 ">
       <Login></Login>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var">
+    <div className="dark:bg-zinc-950 bg-slate-200 min-h-screen">
       hello world
     </div>
   );
