cc159c1159b540fd05d9114d210cf01656237fd1
yash2154 <yash2154rai@gmail.com>
2024-09-17 11:34:37 +0530

    fix bg in sigup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import SignUp from "@/components/signupcomponent";
 
 export default function signup() {
   return (
-    <main className=" h-screen w-screen flex items-center justify-center">
+    <main className=" h-screen w-screen flex items-center justify-center dark:bg-zinc-950 bg-slate-200">
       <SignUp></SignUp>
     </main>
   );
