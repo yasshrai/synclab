26d4d864f416d4d4ce089fd65eb3ae6e4c0b17c7
yash2154 <yash2154rai@gmail.com>
2024-09-27 13:02:54 +0530

    add auth for not accessing different user project

--- src/app/(main)/dashboard/[uid]/[projectname]/page.tsx
+++ src/app/(main)/dashboard/[uid]/[projectname]/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+
 interface ProjectProps {
   params: {
     uid: string;
@@ -9,6 +14,22 @@ interface ProjectProps {
 
 export default function Project({ params }: ProjectProps) {
   const { uid, projectname } = params;
+  const [user, loading] = useAuthState(auth);
+  const { toast } = useToast();
+
+  const router = useRouter();
+  if (uid != user?.uid) {
+    router.push("/dashboard");
+    toast({
+      variant: "default",
+      className: "text-white font-bold bg-red-700",
+      title: "You cannot access this project",
+      duration: 4000,
+    });
+  }
+  if (loading) {
+    <div>loading..</div>;
+  }
 
   return (
     <div className="text-3xl text-white overflow-x-hidden ">
